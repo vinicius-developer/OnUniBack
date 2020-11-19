e3430f9f6c063b5641786b7f616316bf3c47038c
Vinicius de Paula <viniciusilvav1@hotmail.com>
2020-11-19 18:08:09 -0300

    altareção seeder causas sociais

--- database/seeders/CausasSociaisTableSeeder.php
+++ database/seeders/CausasSociaisTableSeeder.php
@@ -15,19 +15,39 @@ class CausasSociaisTableSeeder extends Seeder
     public function run()
     {
         CausaSocial::create([
-            'nome_causa_social' => 'Auxílio a desabrigados'
+            'nome_causa_social' => 'Proteção Ambiental'
         ]);
 
         CausaSocial::create([
-            'nome_causa_social' => 'Auxílio a crianças'
+            'nome_causa_social' => 'Educação Infantil'
         ]);
 
         CausaSocial::create([
-            'nome_causa_social' => 'Cuidados a deficientes fisícos'
+            'nome_causa_social' => 'Tratamento Médico'
         ]);
 
         CausaSocial::create([
-            'nome_causa_social' => 'Auxílio a idosos'
+            'nome_causa_social' => 'Proteção Animal'
+        ]);
+
+        CausaSocial::create([
+            'nome_causa_social' => 'Casa Solidária'
+        ]);
+
+        CausaSocial::create([
+            'nome_causa_social' => 'Refugiados'
+        ]);
+
+        CausaSocial::create([
+            'nome_causa_social' => 'Doação de Alimento'
+        ]);
+
+        CausaSocial::create([
+            'nome_causa_social' => 'Violência Doméstica'
+        ]);
+
+        CausaSocial::create([
+            'nome_causa_social' => 'Outros'
         ]);
     }
 }
